a5f4cbc5b553dc337aebfce06a8b2d6b2b274e87
KimYoungmin <snskzh@naver.com>
2025-04-07 09:21:27 +0900

    Spring boot initializr

---
+++ HashTag_BE/src/main/java/com/example/HashTag/HashTagApplication.java
@@ -0,0 +1,13 @@
+package com.example.HashTag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashTagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HashTagApplication.class, args);
+	}
+
+}
