116b25f1736dfe49f6b9705a82b1292932d62008
KimYoungmin <snskzh@naver.com>
2025-04-10 11:24:52 +0900

    update .gitignore

--- HashTag_BE/src/main/java/com/example/HashTag/Chat/db/ChatRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.HashTag.Chat.db;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.nio.charset.CharsetEncoder;
-
-public interface ChatRepository extends JpaRepository<ChatEntity, Long> {
-}
